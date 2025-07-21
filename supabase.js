8e673619677e9ba6b857a3fd568d45a3db9d95c3
rizaxx12 <sukmasharjaya@gmail.com>
2025-07-21 14:39:19 +0700

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,5 @@
+// supabase.js
+const SUPABASE_URL = "https://rfnnerowmgpshdeqqpon.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmbm5lcm93bWdwc2hkZXFxcG9uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMwNTI1NzgsImV4cCI6MjA2ODYyODU3OH0.dbm5Jh4FcfJmnje_GYkkAbFBxoifz15Dk58_xO77Au8";
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
